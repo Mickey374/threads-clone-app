79ea48ec94591b8f196a6674035b4ac92c522332
Mickey374 <asantemichael37483@gmail.com>
2023-08-19 01:20:50 +0200

    Created the Profile tabs and fetch User specific content in shadcn tabs.

--- application/components/cards/ThreadCard.tsx
+++ application/components/cards/ThreadCard.tsx
@@ -37,14 +37,18 @@ const ThreadCard = ({
   isComment,
 }: Props) => {
   return (
-    <article className={`flex w-full flex-col rounded-${isComment ? 'px-0 xs:px-7 mt-5' : 'bg-dark-2 p-7 mt-10'} `}>
+    <article
+      className={`flex w-full flex-col rounded-xl ${
+        isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
+      } `}
+    >
       <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 flex-row gap-4">
           <div className="flex flex-col items-center">
             <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
               <Image
                 src={author.image}
-                alt="Profile Image"
+                alt="user_community_image"
                 fill
                 className="cursor-pointer rounded-full"
               />
@@ -60,7 +64,9 @@ const ThreadCard = ({
             </Link>
             <p className="mt-2 text-small-regular text-light-2">{content}</p>
 
-            <div className="mt-5 flex-col gap-3">
+            <div
+              className={`${isComment && "mb-10"} mt-5 flex flex-col gap-3}`}
+            >
               <div className="flex gap-3.5">
                 <Image
                   src="/assets/heart-gray.svg"
@@ -97,7 +103,9 @@ const ThreadCard = ({
 
               {isComment && comments.length > 0 && (
                 <Link href={`/thread/${id}`}>
-                    <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+                  </p>
                 </Link>
               )}
             </div>
