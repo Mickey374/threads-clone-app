3fa0b0acb23f5167ae625bfbc583afa16fdefaba
Mickey374 <asantemichael37483@gmail.com>
2023-08-27 16:22:38 +0200

    Added the total fetchUsers on a thread post as reply.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,28 @@
 // "use client"
-import ThreadCard from "@/components/cards/ThreadCard";
-import { fetchPosts } from "@/lib/actions/thread.action";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default async function Home() {
+import ThreadCard from "@/components/cards/ThreadCard";
+import Pagination from "@/components/shared/Pagination";
+
+import { fetchPosts } from "@/lib/actions/thread.action";
+import { fetchUser } from "@/lib/actions/user.action";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
-  const result = await fetchPosts(1, 30);
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchPosts(
+    searchParams.page ? +searchParams.page : 1,
+    30
+  );
 
   return (
     <>
@@ -20,7 +37,7 @@ export default async function Home() {
               <ThreadCard
                 key={post._id}
                 id={post._id}
-                currentUserId={user?.id || ""}
+                currentUserId={user?.id}
                 parentId={post.parentId}
                 content={post.text}
                 author={post.author}
@@ -32,6 +49,12 @@ export default async function Home() {
           </>
         )}
       </section>
+
+      <Pagination
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        path="/"
+        isNext={result.isNext}
+      />
     </>
   );
 }
